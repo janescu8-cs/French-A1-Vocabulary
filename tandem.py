c7fa6c645cce66ae060b4efaceb601c78cc4bcde
janescu8-cs <janescu8.cs@gmail.com>
2025-03-21 22:00:01 +0800

    Update tandem.py

--- tandem.py
+++ tandem.py
@@ -1,9 +1,3 @@
 word_data = {
     "Tu as fait quoi ?": ("你做了什麼？", "Tu as fait quoi ce week-end ?"),
-    "améliorer": ("改善、提升", "Je veux améliorer mon français."),
-    "quotidien": ("日常的（陽性）", "C’est mon travail quotidien."),
-    "quotidienne": ("日常的（陰性）", "Ma routine quotidienne commence à 7h."),
-    "soutenir": ("支持、支援", "Je vais toujours te soutenir."),
-    "environnement": ("環境", "Il faut protéger l’environnement."),
-    "déménager": ("搬家", "Nous allons déménager à Paris."),
 }
