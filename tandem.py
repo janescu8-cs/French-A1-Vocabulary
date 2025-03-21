88f7058f6713bb153433209a45b12ba143a8ebb9
janescu8-cs <janescu8.cs@gmail.com>
2025-03-21 22:20:38 +0800

    Update tandem.py

--- tandem.py
+++ tandem.py
@@ -1,9 +1,8 @@
 word_data = {
-    "Tu as fait quoi ?": ("你做了什麼？", "Tu as fait quoi ce week-end ?"),
+    "tu as fait quoi ?": ("你做了什麼？", "Tu as fait quoi ce week-end ?"),
     "rêver": ("做夢、夢想", "Je veux toujours rêver en grand."),
     "bruyant": ("吵鬧的（陽性）", "Le voisin est très bruyant le soir."),
     "bruyante": ("吵鬧的（陰性）", "La rue bruyante m’empêche de dormir."),
     "progresser": ("進步、提升", "Tu as bien progressé en français !"),
     "un conseil": ("建議", "Peux-tu me donner un conseil ?"),
-    "accueillir": ("接待、歡迎", "Ils vont accueillir les invités à 18h."),
 }
