5f8f14c5a7a39e569bb117351a959375e1b16e82
janescu8-cs <janescu8.cs@gmail.com>
2025-03-22 19:55:04 +0800

    Update tandem.py

--- tandem.py
+++ tandem.py
@@ -5,4 +5,11 @@ word_data = {
     "bruyante": ("吵鬧的（陰性）", "La rue bruyante m’empêche de dormir."),
     "progresser": ("進步、提升", "Tu as bien progressé en français !"),
     "un conseil": ("建議", "Peux-tu me donner un conseil ?"),
+    "gentil": ("親切的、友善的（陽性）", "Il est très gentil avec moi."),
+    "gentille": ("親切的、友善的（陰性）", "Elle est toujours gentille avec les enfants."),
+    "l’embouteillage": ("交通堵塞", "Il y a un grand embouteillage sur l’autoroute."),
+    "envoyer": ("寄送、發送", "Je vais envoyer un email ce soir."),
+    "quotidien": ("每日的；日常的（陽性）", "C’est mon journal quotidien."),
+    "quotidienne": ("每日的；日常的（陰性）", "Ma routine quotidienne commence à 7 heures."),
+    "améliorer": ("改善、提升", "Je veux améliorer mon français."),
 }
