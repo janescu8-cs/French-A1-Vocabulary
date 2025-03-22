5008b044769116e1feb3bfe31b1f39e254b04849
janescu8-cs <janescu8.cs@gmail.com>
2025-03-22 20:23:36 +0800

    Update duolingo.py

--- duolingo.py
+++ duolingo.py
@@ -1,2 +1,12 @@
 word_data = {
+    "le porc": ("豬肉", "Le porc est souvent utilisé dans la cuisine asiatique."),
+    "le bœuf": ("牛肉", "Je préfère le bœuf au poulet."),
+    "la sauce": ("醬汁", "Cette sauce est parfaite avec les pâtes."),
+    "le poivron": ("甜椒", "J’ai acheté un poivron rouge pour la salade."),
+    "un légume jaune": ("一種黃色的蔬菜", "Le maïs est un légume jaune courant."),
+    "camembert": ("卡門貝爾起司（法國軟乳酪）", "Le camembert se mange souvent avec du pain."),
+    "accompagner": ("陪伴、搭配", "Ce vin peut accompagner le fromage."),
+    "la vache": ("母牛", "La vache produit du lait."),
+    "cuit": ("煮熟的", "Le poisson est bien cuit."),
+    "cuisiner": ("烹飪", "J’aime cuisiner pour ma famille."),
 }
